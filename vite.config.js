c400fff728fffd47dd066d9b472dada79d4e25dc
Anand <anand2008murali1@gmail.com>
2025-12-02 09:53:48 +0530

    Fix GitHub pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sics-offiical/",   // <-- IMPORTANT FOR GITHUB PAGES
+  base: "/sics-official/",   // <-- EXACT REPO NAME
   plugins: [react()],
 })
